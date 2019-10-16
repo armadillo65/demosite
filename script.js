ee33365804612864e8d0a20c32008656189f6133
armadillo65 <51421204+armadillo65@users.noreply.github.com>
2019-10-15 17:34:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,27 +7,17 @@ $(document).ready(function(){
    $('body,html').animate({scrollTop: $(this.hash).offset().top-75}, 1000)
   }); // end smooth scroll
 
-  // var winWd = window.innerWidth;
-  // console.log(winWd);
-
-// --- Window (main) Scroll func.---
+// --- Window Scroll func.---
   $(window).scroll(function(){
 
     const nav = document.querySelector('nav');
     const img = document.querySelector('.navCara');
     const main = document.querySelector('main');
-    // window scrolling place nav at top
+    // window scrolling place nav at top, add shadow
     if ($(window).scrollTop() >= 50) {
-      // img.style.display= "none";
-      // $(img).fadeOut(500);
       nav.classList.add('fixed-top');
-      // nav.style.display="inherit";
       $(nav).css({'box-shadow': '0px 0px 10px #555'});
-    }
-    else if($(window).scrollTop() <= 30){
-      // img.style.display= "inherit";
-      // img.style.height= "60vh";
-    }
+    };
 
 // --- window scrolling Add Active Nav Class ---
   var scrollbarLocation = $(this).scrollTop();
@@ -42,34 +32,11 @@ $(document).ready(function(){
   }); // end scrollLink func.
 }); // end window (main) scroll function
 
-// --- nav float away bar ---
-/*
-var prevScrPos = window.pageYOffset;
-
-window.onscroll = function(){
-
-  var currScrPos = window.pageYOffset;
-
-  if(prevScrPos > currScrPos || currScrPos <= 0){
-    document.querySelector('#navbar').style.top = "0";
-  }
-  else if (window.pageYOffset < 0){
-    document.querySelector('#navbar').style.top = "0";
-  }
-  else {
-    document.querySelector('#navbar').style.top = "-100px";
-  }
-  prevScrPos = currScrPos;
-};
- // end nav float func ---
-*/
 });//end of JQuery doc ready
 
-
-// --- nav in viewport mode --
-const team = document.querySelector('#navbarSupportedContent ul');
+// --- Menu collapse after target selected ---
+const navB = document.querySelector('#navbarSupportedContent ul');
 const navDrop = document.querySelector('#navbarSupportedContent');
-team.addEventListener('click',function(){
+navB.addEventListener('click',function(){
   navDrop.classList.toggle('show');
-  console.log("d", navDrop)
-})
+});
